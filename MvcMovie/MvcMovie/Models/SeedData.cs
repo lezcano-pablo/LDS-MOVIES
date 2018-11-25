359bb6040c51290d208f7b2f983c9df661f95b07
Familia\FamiliaLezcano <pablolezcano1984@gmail.com>
2018-11-24 23:16:46 -0300

    Changes

---
+++ MvcMovie/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+           
+              
+            }
+        }
+    }
